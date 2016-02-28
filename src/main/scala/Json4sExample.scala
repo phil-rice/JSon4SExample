df87ce516b4c005c80c49bab964c16a9f3962a83
Phil <phil.rice@iee.org>
2016-02-28 10:32:33 +0000

    Cleaning up

--- src/main/scala/Json4sExample.scala
+++ src/main/scala/Json4sExample.scala
@@ -1,28 +1,97 @@
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 
+import org.json4s.Extraction._
 import org.json4s.JsonAST.JString
 import org.json4s._
 import org.json4s.native.Serialization
 
-
-//Utter Gash code: Just playing around to get this to compile and sort of work
-
-object MySerializer {
-
+object Json4Adapter {
   val dateTimeFormat = DateTimeFormatter.ISO_DATE_TIME
+
+  implicit def offsetDateTimeToJObject(o: OffsetDateTime) = new JString(dateTimeFormat.format(o))
+
+  object DateTimeSerializer extends CustomSerializer[OffsetDateTime](formats => ( {
+    case JString(s) => OffsetDateTime.parse(s, dateTimeFormat)
+  }, {
+    case d: OffsetDateTime => JString(dateTimeFormat.format(d))
+  }))
+
+
+  implicit def toPimper(j: JValue) = new OurPimper(j)
+
+  class OurPimper(j: JValue) {
+    def ~(right: (String, JValue)): JValue = j match {
+      case jo: JObject => JObject(jo.obj :+ JField(right._1, right._2))
+    }
+  }
+
+  val formats = DefaultFormats + DateTimeSerializer
 
 }
 
-object DateTimeSerializer extends CustomSerializer[OffsetDateTime](formats => ( {
-  case JString(s) => OffsetDateTime.parse(s, MySerializer.dateTimeFormat)
-}, {
-  case d: OffsetDateTime => JString(MySerializer.dateTimeFormat.format(d))
-}))
+trait Json4Adapter[X] {
+  def serializer: CustomSerializer[X]
+}
+
 
 case class Image(height: Int, width: Int, caption: Option[String])
 
 case class PhotoGalleryImage(mainImage: Image, thumbnail: Image)
+
+object PhotoGalleryImage {
+
+  import Json4Adapter._
+
+  object PhotoGalleryImageCustomerSerializer extends CustomSerializer[PhotoGalleryImage](ser =
+    implicit formats =>
+      ( {
+        case (jv: JValue) =>
+          val image = jv.extract[Image]
+          val thumbnail = (jv \ "thumbnail").extract[Image]
+          PhotoGalleryImage(image, thumbnail)
+      }, {
+        case p: PhotoGalleryImage =>
+          decompose(p.mainImage) ~ ("thumbnail" -> decompose(p.thumbnail))
+      })) {
+  }
+
+  val formats = Json4Adapter.formats + PhotoGalleryImageCustomerSerializer
+
+}
+
+object Index {
+
+  import JsonDSL._
+
+  object IndexSerializer extends CustomSerializer[Index](
+    ser = implicit formats => ( {
+      case jv: JValue =>
+        val id = (jv \ "id").extract[String]
+        val field1 = (jv \ "field1").extract[String]
+        val field2 = (jv \ "field2").extract[String]
+        val media = jv \ "media"
+        val offsetDateTime = (jv \ "offsetDateTime").extract[OffsetDateTime]
+        val bodyImages = (media \ "bodyimages").extract[Map[String, Image]]
+        val thumbnailImages = (media \ "thumbnails").extract[Map[String, Image]].values.toList
+        val photoGallery = (media \ "photogallery").extract[Map[String, PhotoGalleryImage]]
+        Index(id, field1, field2, offsetDateTime, bodyImages, thumbnailImages, photoGallery)
+    }, {
+      case i: Index =>
+        import Json4Adapter.offsetDateTimeToJObject
+        ("id" -> i.id) ~
+          ("field1" -> i.field1) ~
+          ("field2" -> i.field2) ~
+          ("offsetDateTime" -> i.offsetDateTime) ~
+          ("media" -> (
+            ("bodyimages" -> Extraction.decompose(i.bodyImages)) ~
+              ("thumbnails" -> Extraction.decompose(i.thumbnailImages.zipWithIndex.map{case (image, i) => (i.toString, image)}.toMap)) ~
+              ("photogallery" -> Extraction.decompose(i.photoGallery))))
+    }))
+
+  val formats = PhotoGalleryImage.formats + IndexSerializer
+
+}
 
 case class Index(
                   id: String,
@@ -34,94 +103,13 @@ case class Index(
                   photoGallery: Map[String, PhotoGalleryImage])
 
 
-object OffsetDateTimeSerializer {
-  val dateTimeFormat = DateTimeFormatter.ISO_DATE_TIME
-  val serializer = new CustomSerializer[OffsetDateTime](formats =>
-    ( {
-      case a: JString => OffsetDateTime.parse(a.s, MySerializer.dateTimeFormat)
-    }, {
-      case o: OffsetDateTime => offsetDateTimeToJObject(o)
-    }
-      )) {
-  }
-
-  implicit def offsetDateTimeToJObject(o: OffsetDateTime) = new JString(MySerializer.dateTimeFormat.format(o))
-}
-
-object Index {
-
-  import org.json4s.JsonDSL._
-  import org.json4s._
-  import Extraction._
-
-  implicit def toPimper(j: JValue) = new OurPimper(j) {
-  }
-
-  class OurPimper(j: JValue) {
-    def ~(right: (String, JValue)): JValue = j match {
-      case jo: JObject => JObject(jo.obj :+ JField(right._1, right._2))
-    }
-  }
-
-  def toJFieldList(x: JValue): List[JField] = x match {
-    case j: JObject => j.obj
-  }
-
-  object PhotoGalleryImageCustomerSerializer extends CustomSerializer[PhotoGalleryImage](ser =
-    implicit formats =>
-      ( {
-        case (jv: JValue) =>
-          val image = jv.extract[Image]
-          val thumbnail = (jv \ "thumbnailImage").extract[Image]
-          PhotoGalleryImage(image, thumbnail)
-      }, {
-        case p: PhotoGalleryImage =>
-          decompose(p.mainImage) ~ ("thumbnail" -> decompose(p.thumbnail))
-      })) {
-  }
-
-
-  import org.json4s._
-  import org.json4s.native.JsonMethods._
-  import org.json4s.JsonDSL._
-  import java.util.UUID
-
-  implicit val formats = DefaultFormats + DateTimeSerializer + OffsetDateTimeSerializer.serializer
-
-  object IndexSerializer extends CustomSerializer[Index](
-    ser = implicit formats => ( {
-      case jv: JValue =>
-        val id = (jv \ "id").extract[String]
-        val field1 = (jv \ "field1").extract[String]
-        val field2 = (jv \ "field2").extract[String]
-        val media = jv \ "media"
-        val offsetDateTime = (jv \ "offsetDateTime").extract[OffsetDateTime]
-        val bodyImages = (media \ "bodyimages").extract[Map[String, Image]]
-        val thumbnailImages = (media \ "thumbnails").extract[List[Image]]
-        val photoGallery = (media \ "photogallery").extract[Map[String, PhotoGalleryImage]]
-        Index(id, field1, field2, offsetDateTime, bodyImages, thumbnailImages, photoGallery)
-    }, {
-      case i: Index =>
-        import OffsetDateTimeSerializer.offsetDateTimeToJObject
-        ("id" -> i.id) ~
-          ("field1" -> i.field1) ~
-          ("field2" -> i.field2) ~
-          ("offsetDateTime" -> i.offsetDateTime) ~
-          ("media" -> (
-            ("bodyimages" -> Extraction.decompose(i.bodyImages)) ~
-            ("thumbnails" -> Extraction.decompose(i.thumbnailImages)) ~
-            ("photogallery" -> Extraction.decompose(i.photoGallery))))
-    }))
-
-
-}
-
 object Doit {
 
-  import Index.formats
 
   def main(args: Array[String]) {
     import org.json4s.native.JsonMethods._
+    implicit val formats = Index.formats
+
     val pg = PhotoGalleryImage(Image(10, 20, Some("main")), Image(30, 40, Some("thumbnail")))
     val image1 = Image(10, 20, Some("one"))
     val index = Index("someId", "some1", "some2", OffsetDateTime.now, Map("one" -> image1), List(image1), Map("xx" -> pg))
